0bb64429fb72c482005289c92ee4a097ab511efe
堀家隆宏 <horike@horiketakahiro-no-MacBook-Pro.local>
2016-06-05 19:36:42 +0900

    some update

--- README.md
+++ README.md
@@ -1,113 +1,18 @@
 # Simple GA Ranking 
-Contributors: horike,megumithemes
-Tags:  form, ranking, popular, google analytics
-Requires at least: 3.6.1
-Tested up to: 4.4
-Stable tag: 2.0.3
+## Overview
+This is a WordPress plugin that shows articles ranking using Google Analytics.
+The feature to work very lightweight, because it is not save ranking data in WordPress DB.
 
-Ranking plugin using data from google analytics.
+## How to use
+Please show [the official site](http://simple-ga-ranking.org/)
 
-# Description
-
-Ranking plugin using data from google analytics.
-
-= How use =
-1. Set your Google analytics account and so on at the Simple GA Ranking's option page (Settings->Simple GA Ranking).
-
-2. Put the short code at where you'd like to show the ranking.
-
-3. You can use many kind of filters by post-type or taxonomy. For example, you can show only posts and pages by `[sga_ranking post_type="post,page"]`. Also, showing posts in only WordPress category by `[sga_ranking post_type="post" category__in="wordpress"]`.
-
-4. The short code supports parameters as follows: `post_type`, `exclude_post_type`, `'taxonomy_slug'__in`, `'taxonomy_slug'__not_in`, filter. All parameters except “filter” need to be set the slug(s).
-
-- post_type:  set the attribute of the slug of the post-type you’d like to show. You can set multiple sulgs separeted by comma.
-
-- exclude_post_type: set the attribute of the post-type you’d NOT like to show. You can set multiple sulgs separeted by comma.
-
-- 'taxonomy_slug'__in: set the attribute of the taxonomy you’d like to show. You can set multiple terms separeted by comma.
-
-- 'taxonomy_slug'__not_in: set the attribute of the taxonomy you’d NOT like to show. You can set multiple terms separeted by comma.
-
-- filter:  You can use the filter parameter as same as the fileter parameter of Google Analytics API.
-[https://developers.google.com/analytics/devguides/reporting/core/v3/reference](https://developers.google.com/analytics/devguides/reporting/core/v3/reference)
-
-For example, suppose you have a multi-languages site with direcotry based multisite, and take the statistic of all sites of the multisite having with the URL structure as follows by one GA account.
-http://example.com/ja
-http://example.com/en
-http://example.com/cn
-
-When you’d like to take the statistic of each site, revise like below.
-[sga_ranking filter="pagePath=~^/ja/"]
-[sga_ranking filter="pagePath=~^/en/"]
-[sga_ranking filter="pagePath=~^/cn/"]
-
-Please try ohter parameters using the below sites as a reference.
-[https://support.google.com/analytics/answer/1034324?hl=en](https://support.google.com/analytics/answer/1034324?hl=en)
-[https://developers.google.com/analytics/devguides/reporting/core/v3/reference?hl=en](https://developers.google.com/analytics/devguides/reporting/core/v3/reference?hl=en)
-
-You can use JSON REST API Endpoint. Require plugin [JSON REST API](https://wordpress.org/plugins/json-rest-api/)
-`http://example.com/wp-json/ranking`
-`http://example.com/wp-json/ranking?filter[display_count]=10`
-`http://example.com/wp-json/ranking?filter[period]=7`
-
-
-# Translators
+## Translators
 * English(en) - [megumithemes](http://profiles.wordpress.org/megumithemes/)
 * Japanese(ja) - [Horike Takahiro](http://profiles.wordpress.org/horike)
 * Thai(th_TH) - [TG Knowledge](http://www.xn--12cg1cxchd0a2gzc1c5d5a.com)
 
-You can send your own language pack to me.
-
-Please contact to me.
-
-* @[horike37](http://twitter.com/horike37) on twitter
-* [Horike Takahiro](https://www.facebook.com/horike.takahiro) on facebook
-
-# Contributors
+## Contributors
 * [Horike Takahiro](http://profiles.wordpress.org/horike)
 * [webnist](https://profiles.wordpress.org/webnist)
 * [TG Knowledge](http://www.xn--12cg1cxchd0a2gzc1c5d5a.com)
 
-# Installation
-
-1. Upload `simple-ga-ranking` to the `/wp-content/plugins/` directory.
-2. Activate the plugin through the 'Plugins' menu in WordPress.
-
-# Changelog
-= 1.0 =
-* first release. 
-= 1.1 =
-* Apply widget.
-= 1.2.2 =
-* Metric change visitors to pageviews.
-* fixed a bug that custom post type.
-= 1.2.3 =
-* Add filter parameter.
-= 1.2.4 =
-* Add test mode.`define('SGA_RANKING_TEST_MODE', true);` on wp-config.php.
-= 1.2.5 =
-* fixed a bug that ver 1.2.4
-= 1.2.6 =
-* test mode can set on option page.
-= 1.2.9 =
-* Update readme.txt.
-= 1.2.12 =
-* add filter `sga_widget_shortcode_argument`.
-= 1.2.14 =
-* Thai support
-= 1.2.15 =
-* some fix
-= 1.2.16 =
-* some fix
-= 1.3 =
-* add JSON REST API Endpoint
-= 1.3.1 =
-* some fix
-= 2.0 =
-* OAuth2 authorization available. ClientLogin is no longer available.
-= 2.0.1 =
-* Add some hook.
-= 2.0.2 =
-* some fix
-= 2.0.3 =
-* Add some hook.
---
+++ _site/admin/admin.php
@@ -0,0 +1,156 @@
+<?php
+
+add_action( 'admin_menu', 'sga_ranking_admin_menu' );
+
+function sga_ranking_admin_menu() {
+	add_options_page( __( 'Simple GA Ranking', SGA_RANKING_DOMAIN ), __( 'Simple GA Ranking', SGA_RANKING_DOMAIN ), 'manage_options', 'sga_ranking', 'sga_ranking_options_page');
+}
+
+function sga_ranking_options_page() {
+?>
+<div class="wrap">
+<?php screen_icon(); ?>
+
+<h2><?php _e( 'Simple GA Ranking', SGA_RANKING_DOMAIN ); ?></h2>
+
+<form action="options.php" method="post" id="sga-post">
+<?php settings_fields( 'sga_ranking_options' ); ?>
+<?php do_settings_sections( 'sga_ranking' ); ?>
+<?php do_meta_boxes( 'sga_ranking', 'advanced', '' ); ?>
+
+<p class="submit"><input name="Submit" type="submit" value="<?php _e( 'save', SGA_RANKING_DOMAIN ) ?>" class="button-primary" /></p>
+</form>
+
+</div>
+<?php
+}
+
+add_action( 'admin_init', 'sga_ranking_admin_init' );
+
+function sga_ranking_admin_init() {
+	register_setting( 'sga_ranking_options', 'sga_ranking_options', 'sga_ranking_options_validate' );
+
+	add_settings_section( 'sga_ranking_main', __( 'Configuration', SGA_RANKING_DOMAIN ), 'sga_ranking_section_text', 'sga_ranking' );
+
+//	add_settings_field( 'sga_ranking_email', __( 'E-Mail', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_email',
+//		'sga_ranking', 'sga_ranking_main' );
+
+//	add_settings_field( 'sga_ranking_pass', __( 'Password', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_pass',
+//		'sga_ranking', 'sga_ranking_main' );
+
+//	add_settings_field( 'sga_ranking_profile_id',  __( 'Profile ID', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_profile_id',
+//		'sga_ranking', 'sga_ranking_main' );
+
+//	add_settings_field( 'sga_ranking_start_date', __( 'Start Date', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_start_date',
+//		'sga_ranking', 'sga_ranking_main' );
+		
+//	add_settings_field( 'sga_ranking_end_date', __( 'End Date', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_end_date',
+//		'sga_ranking', 'sga_ranking_main' );
+
+//	add_settings_field( 'sga_ranking_domain', __( 'Domain', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_domain',
+//		'sga_ranking', 'sga_ranking_main' );
+
+//	add_settings_field( 'sga_ranking_pagePath', __( 'pagePath', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_pagePath',
+//		'sga_ranking', 'sga_ranking_main' );
+
+	add_settings_field( 'sga_ranking_period', __( 'Period to get the ranking from today', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_period',
+		'sga_ranking', 'sga_ranking_main' );
+
+	add_settings_field( 'sga_ranking_display_count', __( 'Display Count', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_display_count',
+		'sga_ranking', 'sga_ranking_main' );
+		
+	add_settings_field( 'sga_ranking_debug_mode', __( 'Debug Mode', SGA_RANKING_DOMAIN ), 'sga_ranking_setting_debug_mode',
+		'sga_ranking', 'sga_ranking_main' );
+
+}
+
+function sga_ranking_section_text() {
+}
+
+function sga_ranking_setting_email() {
+	$options = get_option( 'sga_ranking_options' );
+
+	echo '<input id="sga_ranking_email" name="sga_ranking_options[email]" size="40" type="text" value="' . esc_attr( $options['email'] ) . '" />';
+}
+
+function sga_ranking_setting_pass() {
+	$options = get_option( 'sga_ranking_options' );
+
+	echo '<input id="sga_ranking_pass" name="sga_ranking_options[pass]" size="40" type="password" value="' . esc_attr( $options['pass'] ) . '" />';
+}
+
+function sga_ranking_setting_profile_id() {
+	$options = get_option( 'sga_ranking_options' );
+
+	echo '<input id="sga_ranking_user_profile_id" name="sga_ranking_options[profile_id]" size="40" type="text" value="' . esc_attr( $options['profile_id'] ) . '" />';
+}
+
+function sga_ranking_setting_start_date() {
+	$options = get_option( 'sga_ranking_options' );
+	
+	echo '<input id="sga_ranking_start_date" name="sga_ranking_options[start_date]" size="40" type="text" value="' . esc_attr( $options['start_date'] ) . '" /> (YYYY-MM-DD)';
+}
+
+function sga_ranking_setting_end_date() {
+	$options = get_option( 'sga_ranking_options' );
+	
+	echo '<input id="sga_ranking_end_date" name="sga_ranking_options[end_date]" size="40 type="text" value="' . esc_attr( $options['end_date'] ) . '" /> (YYYY-MM-DD)';
+}
+
+function sga_ranking_setting_domain() {
+	$options = get_option( 'sga_ranking_options' );
+	
+	echo 'http://<input id="sga_ranking_domain" name="sga_ranking_options[domain]" size="40" type="text" value="' . esc_attr( $options['domain'] ) . '" />';
+}
+
+function sga_ranking_setting_pagePath() {
+	$options = get_option( 'sga_ranking_options' );
+	
+	echo '<input id="sga_ranking_pagePath" name="sga_ranking_options[pagePath]" size="40" type="text" value="' . esc_attr( $options['pagePath'] ) . '" />';
+}
+
+function sga_ranking_setting_period() {
+	$options = get_option( 'sga_ranking_options' );
+	
+	echo '<input id="sga_ranking_period" name="sga_ranking_options[period]" size="4" type="text" value="' . esc_attr( $options['period'] ) . '" /> ' . __( 'day', SGA_RANKING_DOMAIN );
+}
+
+function sga_ranking_setting_display_count() {
+	$options = get_option( 'sga_ranking_options' );
+	
+	echo '<input id="sga_ranking_display_count" name="sga_ranking_options[display_count]" size="4" type="text" value="' . esc_attr( $options['display_count'] ) . '" />';
+}
+
+function sga_ranking_setting_debug_mode() {
+	$options = get_option( 'sga_ranking_options' );
+	
+	echo '<input id="sga_ranking_debug_mode" name="sga_ranking_options[debug_mode]" size="4" type="checkbox" value="1" ' . checked( $options['debug_mode'], 1 , false ) . '" />';
+}
+
+function sga_ranking_options_validate( $input ) {
+	$newinput['email'] = trim( $input['email'] );
+	$newinput['pass'] = trim( $input['pass'] );
+	$newinput['profile_id'] = trim( $input['profile_id'] );
+	$newinput['start_date'] = trim( $input['start_date'] );
+	$newinput['end_date'] = trim( $input['end_date'] );
+	$newinput['domain'] = trim( $input['domain'] );
+	$newinput['pagePath'] = trim( $input['pagePath'] );
+	$newinput['period'] = absint( $input['period'] );
+	$newinput['display_count'] = absint( $input['display_count'] );
+	$newinput['debug_mode'] = absint( $input['debug_mode'] );
+	$newinput = apply_filters( 'sga_ranking_options_validate', $newinput, $input );
+
+	return $newinput;
+}
+
+add_action( 'admin_notices', 'sga_ranking_admin_notice' );
+function sga_ranking_admin_notice() {
+	$token = get_option('gapiwp_token');
+	
+	if ( $token == '' ) {
+		echo '<div class="error">Simple GA Ranking is available OAuth2 authorization. Please set on <a href="'.admin_url('/options-general.php?page=gapiwp-analytics').'" >setting panel</a>. ClientLogin is no longer available. Please see <a href="https://developers.google.com/identity/protocols/AuthForInstalledApps" >this link</a></div>';
+	}
+}
+
+
+?>
