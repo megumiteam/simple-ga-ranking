9bfa9e06356a3084873db06f4c4cb1ddec8ecbd8
Takahiro Horike <horike37@gmail.com>
2016-07-27 21:43:39 +0900

    Update simple-ga-ranking.php

--- simple-ga-ranking.php
+++ simple-ga-ranking.php
@@ -4,7 +4,7 @@ Plugin Name: Simple GA Ranking
 Author: Horike Takahiro
 Plugin URI: http://simple-ga-ranking.org
 Description: Ranking plugin using data from google analytics.
-Version: 2.0.6
+Version: 2.0.7
 Author URI: http://simple-ga-ranking.org
 Domain Path: /languages
 Text Domain:
